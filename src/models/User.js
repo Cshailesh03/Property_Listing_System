5d517f73bb0b31615ef24f9271ad1904d5fc8102
Cshailesh03 <chhalotreshailesh@gmail.com>
2025-06-01 19:56:37 +0530

    Refactor Remove unused code

--- src/models/User.js
+++ src/models/User.js
@@ -47,7 +47,6 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
@@ -60,12 +59,10 @@ userSchema.pre('save', async function(next) {
   }
 });
 
-// Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-// Remove sensitive data when converting to JSON
 userSchema.methods.toJSON = function() {
   const userObject = this.toObject();
   delete userObject.password;
